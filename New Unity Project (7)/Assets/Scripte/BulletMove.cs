6fbd54967da00066a3df426a5b13bb4b581f1f90
HyunMin6278 <50650371+HyunMin6278@users.noreply.github.com>
2019-05-27 12:45:35 +0900

    Create BulletMove.cs

---
+++ New Unity Project (7)/Assets/Scripte/BulletMove.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletMove : MonoBehaviour
+{
+    public string poolItemName = "Bullet";
+    public float moveSpeed = 10f;
+    public float lifeTime = 3f;
+    public float _elapsedTime = 0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime);
+       // transform.position += transform.forward * moveSpeed * Time.deltaTime;
+        if(GetTimer() > lifeTime)
+        {
+            SetTimer();
+            ObjectPool.Instance.PushToPool(poolItemName, gameObject);
+        }
+    }
+
+    float GetTimer()
+    {
+        return (_elapsedTime += Time.deltaTime);
+    }
+
+    void SetTimer()
+    {
+        _elapsedTime = 0f;
+    }
+}
