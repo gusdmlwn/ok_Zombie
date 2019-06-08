2733c7d32dd5f76e72695efb98e119f362228a02
HyunMin6278 <50650371+HyunMin6278@users.noreply.github.com>
2019-06-08 22:01:08 +0900

    Create ItemCtrl.cs

---
+++ New Unity Project (7)/Assets/Scripte/ItemCtrl.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemCtrl : MonoBehaviour
+{
+    public GameObject ItemBox;
+    private GameObject[] WayPoint = new GameObject[8];
+    private int[] RandomNumber = new int[3];
+    private bool[] Flag = new bool[8];
+    // Start is called before the first frame update
+    void Start()
+    {
+        for(int i =0;i<8;i++)
+        {
+            WayPoint[i] = transform.GetChild(i).gameObject;
+        }
+        for (int i = 0; i < 8; i++)
+            Flag[i] = false;
+        StartCoroutine(this.InitItem());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator InitItem()
+    {
+        while(true)
+        {
+            yield return new WaitForSeconds(5.0f);
+
+            int number = Random.Range(0, 8);
+            if (Flag[number] == false)
+            {
+                GameObject itembox = (GameObject)Instantiate(ItemBox, WayPoint[number].transform.position, Quaternion.identity);
+                Flag[number] = true;
+            }
+
+        }
+    }
+}
