7c410fbbdf17f3ade603f6198b4ec273a97ae730
HyunUiJoo <gusdmlwn@naver.com>
2019-06-09 01:49:54 +0900

    Create CreateItemCtrl.cs

---
+++ New Unity Project (7)/Assets/Scripte/CreateItemCtrl.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateItemCtrl : MonoBehaviour
+{
+    public GameObject Coin;
+
+    // Start is called before the first frame update
+    void OnCollisionEnter(Collision coll)
+    {
+        Debug.Log("ItemCollision");
+        if (coll.collider.tag == "Player")
+        {
+            Debug.Log("destroy");
+            GameObject coin = (GameObject)Instantiate(Coin, this.transform.position, Quaternion.identity);
+            Destroy(this.gameObject);
+        }
+    }
+}
